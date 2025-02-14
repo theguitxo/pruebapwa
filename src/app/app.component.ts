ec1283a4814389ecf56f0f319f18257d50362657
Amador Tello Mimbrero <amador.tellomimbrero@emeal.nttdata.com>
2025-02-14 14:03:19 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import * as versionData from '../../public/assets/config.json';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mi-pwa';
+export class AppComponent implements OnInit {
+  httpClient!: HttpClient;
+
+  showUpdateButton = false;
+  currentVersion!: string;
+
+  constructor() {
+    this.httpClient = inject(HttpClient);
+  }
+
+  ngOnInit(): void {
+    this.currentVersion = versionData.version;
+
+    const headers = new HttpHeaders().set('Cache-Control', 'no-cache').set('Pragma', 'no-cache');
+
+    this.httpClient.get<{ version: string }>('./assets/config.json', { headers })
+      .subscribe(config => {
+        console.log(config);
+
+        this.showUpdateButton =config.version !== this.currentVersion;
+      });
+  }
+
+  update(): void {
+    location.reload();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 
-import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideHttpClient()
+  ]
 };
